9758e0814dcecb03949a83e2b9eb6cfa1203483e
shery11 <shery_kirmani@hotmail.com>
2017-08-08 20:32:12 +0500

    validation added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -13,16 +13,38 @@ export class HomePage {
  @ViewChild('moisture') moisture;
 
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
 
   }
 
 
   submit(){
-  	console.log(this.temperature.value);
-  	console.log(this.ph.value);
-  	console.log(this.sunlight.value);
-  	console.log(this.moisture.value);
+  	
+     if(this.temperature.value > 0 && this.temperature.value < 55 && this.ph.value > 0  && this.ph.value < 14  )
+     {
+        console.log(this.temperature.value);
+         console.log(this.ph.value);
+        console.log(this.sunlight.value);
+        console.log(this.moisture.value);
+     }else{
+
+
+          let alert = this.alertCtrl.create({
+          title: 'Error',
+          subTitle: 'Invalid values',
+          buttons: ['OK']
+        });
+        alert.present();
+          
+        this.temperature.value = "";
+        this.ph.value = "";
+        this.sunlight.value = "";
+        this.moisture.value = "";  
+
+
+     }
+
+   
   }
 
 
